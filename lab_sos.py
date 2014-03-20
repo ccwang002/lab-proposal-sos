15ecc934bd63af22ac476d8293e375d618e97867
Liang Bo Wang <ccwang002@gmail.com>
2014-03-21 04:27:31 +0800

    rename python script, easy to debug

--- lab_sos.py
+++ lab_sos.py
@@ -1,23 +1,43 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.wtf import Form
 from flask.ext.script import Manager
+
+from flask.ext.bootstrap import Bootstrap
+
+from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dont let your boss see this'
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+db = SQLAlchemy(app)
 
+
+class Project(db.Model):
+    __tablename__ = 'projs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Unicode(256), index=True)
+    mod_time = db.Column(db.Unicode(128))
+    start_time = db.Column(db.Unicode(128), nullable=True)
+    time_span = db.Column(db.Unicode(128), nullable=True)
+
+    def __repr__(self):
+        return '<Project {}>'.format(self.title)
 
 @app.errorhandler(404)
 def page_not_found(e):
     path = request.path
     return render_template('404.html', path=path), 404
-
 
 @app.route('/')
 def index():
